eebb738ec7add9890dc5c7d55e2a89a3fa52fd34
Tirik123 <claudiocheri00@gmail.com>
2023-06-14 17:28:14 +0200

    edited

--- Altklausuren/WS 22/ex3_strings.py
+++ Altklausuren/WS 22/ex3_strings.py
@@ -1,3 +1,26 @@
 # Aufgabe 3 (Strings) #########################################################
 
-# def is_list_of_int(x):
+def is_list_of_int(x: str) -> bool:
+    if x[0] != "[" or x[-1] != "]" or len(x) < 2:
+        return False
+    
+    splits = x.split(",")
+    #return splits
+    for s in splits:
+        if s.isdigit() == True and s != int:
+            return False
+        if not s.isdigit():
+            return False 
+    return True
+
+        #print(s)
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    print(is_list_of_int("[100,-44,0]"))

7c6dfa22c0c511ec2fac0ef6fa59910b46a12c43
Tirik123 <claudiocheri00@gmail.com>
2023-06-15 22:22:49 +0200

    ed

--- Altklausuren/WS 22/ex6_trees.py
+++ Altklausuren/WS 22/ex6_trees.py
@@ -16,9 +16,46 @@ Tree = Optional[Node]
 
 # (a) #########################################################################
 
-# def mark_at_path(tree, path):
+def mark_at_path(tree: Tree, path: str) -> Optional[str]:
+    if tree is None:
+        return None
+    if path == "":
+        return tree.mark
+    if path[0] == "l":
+        return mark_at_path(tree.left, path[1:])
+    if path[0] == "r":
+        return mark_at_path(tree.right, path[1:])
+    
+    
 
 
 # (b) #########################################################################
 
-# def paths(tree):
+def paths(tree: Tree) -> list[str]:
+    if tree is None:
+        return []
+    ps = [""]
+    for p in paths(tree.left):
+        ps.append("l" + p)
+    for p in paths(tree.right):
+        ps.append("r" + p)
+    return ps
+
+
+
+example = Node("n0",Node("n1", None, None),Node("n2",Node("n3", None, None),Node("n4", None, None)))
+
+
+if __name__ == "__main__":
+    assert mark_at_path(example, "") == "n0"
+    assert mark_at_path(example, "l") == "n1"
+    assert mark_at_path(example, "r") == "n2"
+    assert mark_at_path(example, "rl") == "n3"
+    assert mark_at_path(example, "rr") == "n4"
+    assert mark_at_path(example, "ll") is None
+
+    assert paths(example) == ["", "l", "r", "rl", "rr"]
+
+    
+
+

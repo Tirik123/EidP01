d7a43b8a4dbacf16637cfda4ebc6bd552da1ffbc
Tirik123 <claudiocheri00@gmail.com>
2023-06-17 11:22:43 +0200

    ed

--- Altklausuren/WS 22/ex8_fp.py
+++ Altklausuren/WS 22/ex8_fp.py
@@ -3,14 +3,25 @@
 
 # (a) #########################################################################
 
-# def times_2(f):
+def times_2(f):
+    return lambda x: f(x) * 2
+
 
 
 # (b) #########################################################################
 
-# def map_matrix(f, m):
+def map_matrix(f, m):
+    return [[f(x) for x in row] for row in m]
+
 
 
 # (c) #########################################################################
 
-# def map_matrix_2(f, m):
+def map_matrix_2(f, m):
+    return list(map(lambda row: list(map(f, row)), m))
+
+
+if __name__ == '__main__':
+    f = lambda x: x + 1
+    g = times_2(f)
+    print(g(3))

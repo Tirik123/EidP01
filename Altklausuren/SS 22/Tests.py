d0e6f0d68a19af2116f6ebb4a67ddeda58bd90a4
Tirik123 <claudiocheri00@gmail.com>
2023-06-28 17:19:15 +0200

    ed

---
+++ Altklausuren/SS 22/Tests.py
@@ -0,0 +1,10 @@
+def leapyear(year: int) -> bool:
+    if year % 4 == 0:
+        if year % 100 != 0:
+            return True
+        elif year % 100 == 0 and year % 400 == 0:
+            return True
+        else:
+            return False
+    else:
+        return False

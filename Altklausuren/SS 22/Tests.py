5227b307a91345abaf73762553654a2a5d84cd44
Tirik123 <claudiocheri00@gmail.com>
2023-06-28 17:26:54 +0200

    ed

--- Altklausuren/SS 22/Tests.py
+++ Altklausuren/SS 22/Tests.py
@@ -8,3 +8,23 @@ def leapyear(year: int) -> bool:
             return False
     else:
         return False
+    
+
+def test1():
+    assert leapyear(4) == True
+
+def test2():
+    assert leapyear(400) == True
+
+def test3():
+    assert leapyear(200) == False
+
+def test4():
+    assert leapyear(5) == False
+
+
+if __name__ == '__main__':
+    test1()
+    test2()
+    test3()
+    test4()

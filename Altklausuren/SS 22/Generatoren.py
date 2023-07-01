9e50c77b3f1b8205c134be3acc96bf9297b2b89e
Tirik123 <claudiocheri00@gmail.com>
2023-07-01 18:37:36 +0200

    ed

--- Altklausuren/SS 22/Generatoren.py
+++ Altklausuren/SS 22/Generatoren.py
@@ -6,6 +6,26 @@ def without(xs: Any, ys: Any):       # if xs not in ys: return xs
             yield x
 
 
+def my_split(text):
+    word = ""  # Variable zur Aufnahme der einzelnen Wörter
+    if text == '':
+        return None
+    for char in text:  # Schleife zum Durchlaufen jedes Zeichens im Text
+        if char != " ":  # Wenn das Zeichen kein Leerzeichen ist
+            word += char  # Füge das Zeichen zum aktuellen Wort hinzu
+        elif char == ' ':  # Wenn ein Leerzeichen erreicht wurde und das aktuelle Wort nicht leer ist
+            yield word  # Gib das aktuelle Wort mit yield zurück
+            word = ""  # Setze das Wort zurück, um das nächste Wort zu sammeln  # Wenn am Ende des Textes noch ein nicht-leeres Wort vorhanden ist
+    yield word  # Gib das letzte Wort mit yield zurück
+
+
+
+
+
 
 if __name__ == '__main__':
     print(list(without(range(0, 6), [1, 3]))) == [0, 2, 4, 5]
+
+    s1 = 'this is a sentence'
+    print(list(my_split(s1)))
+    assert list(my_split('')) == []

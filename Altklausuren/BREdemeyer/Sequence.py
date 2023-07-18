49a71936fc140da965e2832557766410e6664dda
Tirik123 <claudiocheri00@gmail.com>
2023-07-18 15:53:02 +0200

    ed

---
+++ Altklausuren/BREdemeyer/Sequence.py
@@ -0,0 +1,14 @@
+def count_iterations(n: int) -> int:    # n > 0, return if f == 1
+    iter = 0
+    while n != 1 or iter == 0:
+        if n % 2 == 0:
+            iter += 1
+            n = n // 2
+        elif n % 2 != 0:
+            iter += 1
+            n = 3 * n + 1
+    return iter
+
+if __name__ == '__main__':
+    print(count_iterations(10))
+    print(count_iterations(1))
